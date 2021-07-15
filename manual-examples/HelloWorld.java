349ea8c226340770e033a9de877d4e7a3e2d4820
CCunningham0 <caleb.cunningham@revature.net>
2021-07-14 22:03:42 -0400

    Added hello world class

---
+++ manual-examples/HelloWorld.java
@@ -0,0 +1,12 @@
+/* 
+To have public class, class name must match file name exactly and a class file can only contain a single
+public class
+*/
+
+public class HelloWorld {
+	// Main method, required if you want to perform actions with java class file
+	public static void main(String[] args) {
+		System.out.println("Hello World.");
+		
+	}
+}
